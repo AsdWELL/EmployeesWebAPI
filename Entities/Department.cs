be45ff080aefc767d173a91c63353c62996bb290
AsdWELL <dimagodov.23@mail.ru>
2025-01-27 14:11:53 +0300

    Add entities

---
+++ Entities/Department.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeesWebAPI.Entities
+{
+    /// <summary>
+    /// Модель отдела
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// Уникальный идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название отдела
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Телефонный номер
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
---
+++ Entities/Passport.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeesWebAPI.Entities
+{
+    /// <summary>
+    /// Модель паспорта
+    /// </summary>
+    public class Passport
+    {
+        /// <summary>
+        /// Уникальный идентификатор
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Тип паспорта
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Номер паспорта
+        /// </summary>
+        public string Number { get; set; }
+    }
+}
